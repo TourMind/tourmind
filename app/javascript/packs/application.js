e1446611a7aed394d71062a34491c2a280b932ad
Betty Hsu <997666aa@gmail.com>
2023-04-18 22:43:49 +0800

    add update css & meeting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "controllers";
-import "@hotwired/turbo-rails";
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "controllers"
+import "@hotwired/turbo-rails"
+//FONTAWESOME
+import "scripts"
 
 Rails.start();
 Turbolinks.start();
