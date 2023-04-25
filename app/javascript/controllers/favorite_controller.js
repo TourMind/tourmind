3dcd9a26109bee6a77738a89c592acb209e33ebb
tiger <tiger850109@gmail.com>
2023-04-25 13:29:04 +0800

    finish basic section filter

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -2,18 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "section" ]
-
-  connect() {
-    console.log("test")
-  }
   
   toggle(event) {
     event.preventDefault()
     const sectionName = event.target.dataset.section
-    console.log(sectionName)
     this.sectionTargets.forEach(section => {
-      console.log(section)
-      if (section.dataset.section === sectionName) {
+      if (section.dataset.section === sectionName || sectionName == "all") {
         section.classList.remove('hidden')
       } else {
         section.classList.add('hidden')
