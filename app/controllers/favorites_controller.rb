3bcc6658bca29528c47f056bd0c7a392f9195dc0
tiger <tiger850109@gmail.com>
2023-04-26 17:25:11 +0800

    fix redirect to top of the page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,22 +6,21 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = current_user.favorites.new(favorite_params)
-    
+
     if @favorite.save
-      # p "---------------------------------------------------------------------------------"
-      # p "#{request.env['HTTP_REFERER']}##{@favorite.favorable_id}"
-      # p "---------------------------------------------------------------------------------"
-      # redirect_to "#{request.env['HTTP_REFERER']}##{@favorite.favorable_id}"
-      redirect_back fallback_location: root_path, notice: '新增到喜愛清單'
+      # redirect_to restaurants_path(anchor: @favorite.favorable_id)
+      redirect_to "#{request.referrer}##{@favorite.favorable_id}", scroll: {x: 0, y: page_scroll_position}, notice: '新增到喜愛清單'
+      # redirect_back fallback_location: root_path, notice: '新增到喜愛清單'
     else
-      redirect_back fallback_location: root_path, alert: '收藏失敗'
+      # redirect_back fallback_location: root_path, alert: '收藏失敗'
+      redirect_to "#{request.referrer}##{@favorite.favorable_id}", alert: '收藏失敗'
     end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    redirect_back fallback_location: root_path, notice: '以從喜愛清單中移除'
+    redirect_to "#{request.referrer}##{@favorite.favorable_id}", notice: '以從喜愛清單中移除'
   end
 
   private
