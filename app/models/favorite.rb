d339cf25de6a2af84e11de0c2e60e7b4c30c335d
tiger <tiger850109@gmail.com>
2023-04-27 17:36:38 +0800

    index page for favorite list
    index page for favorite list

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,8 +2,10 @@ class Favorite < ApplicationRecord
   belongs_to :user, inverse_of: :favorites
   belongs_to :favorable, polymorphic: true
 
-  # scope :hotels, -> { where(favorable_type: "Hotel") }
+  scope :hotels, -> { where(favorable_type: "Hotel") }
   scope :restaurants, -> { where(favorable_type: 'Restaurant') }
+  scope :sites, -> { where(favorable_type: "Site") }
+  scope :plans, -> { where(favorable_type: "Plan") }
 
   validates :user_id, uniqueness: {
     scope: %i[favorable_id favorable_type],
