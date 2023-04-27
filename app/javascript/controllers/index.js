90391610b36cd0b4d4fdea06b8a68706a9e971d6
YuTingLan <fortitude0623@gmail.com>
2023-04-28 01:12:47 +0800

    add navbar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@ import { application } from "./application"
 
 import FlashController from "./flash_controller"
 application.register("flash", FlashController)
+
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
---
+++ app/javascript/scripts/fontawesome.js
@@ -0,0 +1,7 @@
+//FONT AWSOME
+import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
+config.mutateApproach = 'sync'
+import { faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe)
+dom.watch()
