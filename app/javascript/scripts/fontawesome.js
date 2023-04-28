0cc554655f8922a65bdcbf4c242666eb8c5de09f
Betty Hsu <997666aa@gmail.com>
2023-04-28 23:46:19 +0800

    remove helper file and fixed restaurant model before rubocop

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -1,7 +1,7 @@
 //FONT AWSOME
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 config.mutateApproach = 'sync'
-import { faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons'
 
-library.add( faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe)
+library.add( faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe)
 dom.watch()
