955579e41a0a97637874a34264b9c8f3f09f50e0
YuTingLan <127638342+YuTingLan@users.noreply.github.com>
2023-04-29 12:07:18 +0800

    Neweb pay (#35)
    * modify flash position
    
    * add pricing view
    
    * add newebpay service / WIP
    
    * WIP
    
    * add payment model and paymentok.html.erb
    
    * disable config.hosts and used rubocop to clean up code
    
    * fixed can not found current_user issue
    
    * disable config.hosts
    
    * used rubocop
    
    * rename payment model to order and modify order search base on user
    
    * fixed taillwind issue
    
    * used rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   root 'page#home'
-  # resources :passwords, only: [:edit, :update]
 
   devise_for :users, controllers: {
     registrations: 'users/registrations',
@@ -13,4 +12,14 @@ Rails.application.routes.draw do
   resources :restaurants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :comments
+
+  # 升級方案
+  get '/pricing', to: 'page#pricing'
+  # 金流路徑
+  post '/pricing/return', to: 'page#return'
+  post '/pricing/notify', to: 'page#notify'
+  # 付款成功
+  get '/pricing/paymentok', to: 'page#paymentok'
+  # 訂單資訊
+  get '/pricing/oder', to: 'page#oder'
 end
