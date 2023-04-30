af0ce999cfa526674e4ec0e554f1f36b0c863d0d
Betty Hsu <997666aa@gmail.com>
2023-04-30 23:43:18 +0800

    hotels index layout finished

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -1,7 +1,7 @@
 //FONT AWSOME
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 config.mutateApproach = 'sync'
-import { faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart } from '@fortawesome/free-solid-svg-icons'
 
-library.add( faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe)
+library.add(faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart)
 dom.watch()
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class Hotel < ApplicationRecord
-  validates :name, :tel, :address, presence: true
+  validates :name, :address, presence: true
+  validates :tel, allow_blank: true,
+                  format: { with: /\A(\(\d{2}\)|\d{2})\d{8}\z/, message: '請輸入有效的電話號碼，格式(02)12345678或0912123123' }
+  validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp, message: '請輸入有效的網址' }, if: lambda {
+                                                                                                    website.present?
+                                                                                                  }
 
   def self.search(keyword)
-    where('name LIKE ? OR address LIKE ?', "%#{keyword}%", "%#{keyword}%")
+    where('name LIKE :keyword OR address LIKE :keyword OR hotel_types LIKE :keyword OR equipment::text ILIKE ANY (ARRAY[:keywords])',
+          keyword: "%#{keyword}%", keywords: ["%#{keyword}%"])
   end
   paginates_per 4
   mount_uploader :image, ImageUploader
