a08b37521d04a92c12f386c1980718d94601a949
tiger <tiger850109@gmail.com>
2023-04-30 17:53:30 +0800

    fix conflict

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,5 @@
 import { application } from "./application";
 
-import FlashController from "./flash_controller";
-application.register("flash", FlashController);
-
 import EditController from "./edit_controller";
 application.register("edit", EditController);
 
