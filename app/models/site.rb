96ac2096b337ac26f8a542805b2ba53d46925a11
Betty Hsu <997666aa@gmail.com>
2023-04-30 23:50:19 +0800

    fixed model and view sites index layout finished

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,7 +6,8 @@ class Site < ApplicationRecord
 
   has_many :comments, as: :commentable, dependent: :destroy
   validates :name, :address, presence: true
-  validates :tel, allow_blank: true, format: { with: /\A\(\d{2}\)\d{8}\z/, message: '請輸入有效的電話號碼，格式为(02)12345678' }
+  validates :tel, allow_blank: true,
+                  format: { with: /\A(\(\d{2}\)|\d{2})\d{8}\z/, message: '請輸入有效的電話號碼，格式(02)12345678或0912123123' }
   validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp, message: '請輸入有效的網址' }, if: lambda {
                                                                                                     website.present?
                                                                                                   }
