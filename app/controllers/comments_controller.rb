7d12531c0993feb8cd02c8c6f72d6a2be0aa87ca
chen <zx656084@gmail.com>
2023-05-01 03:53:51 +0800

    comments_related

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,15 +55,4 @@ class CommentsController < ApplicationController
       (5 - rating.to_i).times { stars += '<i class="fa-solid fa-star" style="color: #effc36;"></i>' }
       stars.html_safe
     end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 end
