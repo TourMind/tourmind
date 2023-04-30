ac67fb2ca11926d3082380d8856ed038f7d9af43
tiger <tiger850109@gmail.com>
2023-04-30 19:51:08 +0800

    add link for testing function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,19 +8,19 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.new(favorite_params)
 
     if @favorite.save
-      # redirect_to restaurants_path(anchor: @favorite.favorable_id)
-      redirect_to "#{request.referer}##{@favorite.favorable_id}", notice: '新增到喜愛清單'
-      # redirect_back fallback_location: root_path, notice: '新增到喜愛清單'
+      redirect_back fallback_location: root_path, notice: '新增到喜愛清單'
+      # redirect_to "#{request.referer}##{@favorite.favorable_id}", notice: '新增到喜愛清單'
     else
-      # redirect_back fallback_location: root_path, alert: '收藏失敗'
-      redirect_to "#{request.referer}##{@favorite.favorable_id}", alert: '收藏失敗'
+      redirect_back fallback_location: root_path, alert: '收藏失敗'
+      # redirect_to "#{request.referer}##{@favorite.favorable_id}", alert: '收藏失敗'
     end
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
-    redirect_to "#{request.referer}##{@favorite.favorable_id}", notice: '以從喜愛清單中移除'
+    redirect_back fallback_location: root_path, notice: '以從喜愛清單中移除'
+    # redirect_to "#{request.referer}##{@favorite.favorable_id}", notice: '以從喜愛清單中移除'
   end
 
   private
