c862054f801b6d407deb401a434868208b25ed1b
Betty Hsu <997666aa@gmail.com>
2023-04-30 20:17:37 +0800

    sites layout finished

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -2,7 +2,8 @@
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 config.mutateApproach = 'sync'
 
-import { faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart)
+import { faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faStar, faStarHalf, faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faStar, faStarHalf, faHeart)
 dom.watch()
