6dc88b088441bf1e3c672a1d3186480f1e9043c0
YuTingLan <fortitude0623@gmail.com>
2023-05-01 19:57:19 +0800

    modify path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 
 Rails.application.routes.draw do
   root 'page#home'
+<<<<<<< HEAD
+=======
+  get 'orders/index'
+  get 'orders/show'
+>>>>>>> 1b22483 (modify path)
 
   devise_for :users,
              controllers: {
