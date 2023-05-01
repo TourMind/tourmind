d63d614b880200cbb844d2e0ee7f94b3083b5b4e
Betty Hsu <997666aa@gmail.com>
2023-05-01 17:25:59 +0800

    hotel show layout WIP

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -1,8 +1,8 @@
 //FONT AWSOME
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import { faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart, faBars } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart)
+library.add(faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faStar, faStarHalf, faHeart, faBars)
 dom.watch()
 
