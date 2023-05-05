55b953923e287e5014afc43a5e91ef0574242a70
YuTingLan <fortitude0623@gmail.com>
2023-05-05 15:00:04 +0800

    fixed migration timing conflict.

--- db/schema.rb
+++ db/schema.rb
@@ -41,17 +41,6 @@ ActiveRecord::Schema.define(version: 2023_05_08_090406) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "hotels", force: :cascade do |t|
     t.string "name"
     t.string "website"
@@ -127,7 +116,7 @@ ActiveRecord::Schema.define(version: 2023_05_08_090406) do
     t.string "longitude"
     t.float "stay_duration"
     t.text "intro"
-    t.string "pet_friendly"
+    t.string "pet_freindly"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "site_types", default: [], array: true
@@ -167,4 +156,5 @@ ActiveRecord::Schema.define(version: 2023_05_08_090406) do
   add_foreign_key "favorites", "users"
   add_foreign_key "orders", "users"
   add_foreign_key "plans", "users"
+  add_foreign_key "orders", "users"
 end
