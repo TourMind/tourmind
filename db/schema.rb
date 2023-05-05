084e502ad964f83180f8a4ac18e716a444c609d1
YuTingLan <fortitude0623@gmail.com>
2023-05-05 15:00:04 +0800

    fixed migration timing conflict.

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table 'favorites', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'favorable_type', null: false
-    t.bigint 'favorable_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[favorable_type favorable_id], name: 'index_favorites_on_favorable'
-    t.index ['user_id'], name: 'index_favorites_on_user_id'
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -47,17 +37,6 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
   create_table "hotels", force: :cascade do |t|
     t.string "name"
     t.string "website"
@@ -72,17 +51,16 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
     t.string "hotel_types"
     t.string "tel"
     t.string "equipment", default: [], array: true
-    t.json "images"
   end
 
-  add_foreign_key 'favorites', 'users'
   create_table "orders", force: :cascade do |t|
     t.integer "amount"
     t.datetime "pay_time"
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "plans", force: :cascade do |t|
@@ -114,8 +92,6 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
     t.string "michelin_star"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "slug"
-    t.index ["slug"], name: "index_restaurants_on_slug", unique: true
   end
 
   create_table "sites", force: :cascade do |t|
@@ -126,14 +102,13 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
     t.string "longitude"
     t.float "stay_duration"
     t.text "intro"
-    t.string "pet_friendly"
+    t.string "pet_freindly"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "site_types", default: [], array: true
     t.string "tel"
     t.string "image"
     t.string "parking"
-    t.json "images"
   end
 
   create_table "users", force: :cascade do |t|
@@ -153,8 +128,11 @@ ActiveRecord::Schema.define(version: 2023_05_03_024316) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "diamond_grade", default: "一般會員"
+    t.string "amount"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "users"
+  add_foreign_key "orders", "users"
 end
