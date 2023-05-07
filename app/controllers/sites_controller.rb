3706383b223d5bcc667bfea7bfdd36d1efc21961
Hsu Betty <123808430+Hsubetty@users.noreply.github.com>
2023-05-07 21:07:42 +0800

    Sites  (#52)
    * sites layout
    
    * sites layout WIP
    
    * sites layout finished
    
    * fixed model and view sites index layout finished
    
    * yarn add fontawesome
    
    * sites add filter function
    
    * modify sites model, add images column, rename pet_friendly column
    
    * site show layout-update fake image logic
    
    * rename pet_friendly to sites
    
    * update the filter coloumn 科博館
    
    * update index and remove show 評等
    
    * add goodle map
    
    * rename the site.controller.js to filter.controller.js and run rubocop
    
    * add delete picture function
    
    * add favorite_list logic
    
    * fix site delete bug and add duration validates
    
    * add image validate
    
    * update the text color
    
    * update the error message and view RWD
    
    * update image_uploader and error message
    
    * remove image_uploader minimagic setting and show view
    
    * show all the upload images
    
    * fix: 修正衝突錯誤
    
    * format devise.zh.yml
    
    * fix show sites
    
    * fix show sites
    
    * fix show sites
    
    * add favoorite icon
    
    * index and show add dynamic star and rating by comments
    
    * add parking checkbox in form
    
    * update star and rating bug
    
    * fix image cache
    
    * delete useless images

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,8 +4,18 @@ class SitesController < ApplicationController
   before_action :set_site, only: %i[show edit update destroy]
   helper_method :star_rating
   def index
+    @city_options = %w[台北市 新北市]
+    @site_type_options = %w[自然景觀 歷史文化遺產 美術館 科博館 公園休閒 購物中心 主題樂園 海邊 動物園 體育館 溫泉景點 觀光勝地]
+    @pet_friendly_options = %w[可攜寵物]
+
+    @address = params[:address] || []
+    @site_types = params[:site_types] || []
+    @pet_friendly = params[:pet_friendly] || []
+
     @sites = if params[:keyword].present?
                Site.search(params[:keyword]).order(updated_at: :desc).page(params[:page])
+             elsif @address.present? || @site_types.present? || @pet_friendly.present?
+               Site.filter(@address, @site_types, @pet_friendly).order(updated_at: :desc).page(params[:page])
              else
                Site.order(updated_at: :desc).page(params[:page])
              end
@@ -17,7 +27,7 @@ class SitesController < ApplicationController
   end
 
   def create
-    @site = Site.new(site_parames)
+    @site = Site.new(site_params)
     if @site.save
       redirect_to sites_path, notice: '新增成功'
     else
@@ -26,15 +36,15 @@ class SitesController < ApplicationController
   end
 
   def show
-
     @comment = Comment.new
     @comments = @site.comments
+    @comments_score = @comments.average(:rating).try(:round, 1)
   end
 
   def edit; end
 
   def update
-    if @site.update(site_parames)
+    if @site.update(site_params)
       redirect_to site_path(@site), notice: '更新成功'
     else
       render :edit
@@ -53,9 +63,11 @@ class SitesController < ApplicationController
     @site = Site.find(params[:id])
   end
 
-  def site_parames
-    params.require(:site).permit(:name, :website, :address, :image, :parking, :tel, :latitude, :longitude, :stay_duration, :intro, :pet_freindly, site_types: [])
+  def site_params
+    params.require(:site).permit(:name, :website, :address, :image, :parking, :tel, :latitude, :longitude,
+                                 :stay_duration, :intro, :pet_friendly, :remove_images, :images_cache, site_types: [], images: [],)
   end
+
   def star_rating(rating)
     stars = ''
     if rating.present?
