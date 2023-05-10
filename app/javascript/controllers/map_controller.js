4286ff5468059d2ff5d4b017e7662e4b5fffe0c4
Kevin Yang <e510021@gmail.com>
2023-05-10 12:47:13 +0800

    WIP google map routes

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,11 +12,15 @@ export default class extends Controller {
     });
 
     await loader.load();
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
 
     const locArr = [];
+    const nameArr = [];
 
     this.dayTarget.querySelectorAll(".site").forEach((el) => {
       locArr.push({ lat: +el.dataset.lat, lng: +el.dataset.lng });
+      nameArr.push(el.dataset.name);
     });
 
     const locLat = locArr.map((el) => el.lat).sort((a, b) => a - b);
@@ -31,11 +35,47 @@ export default class extends Controller {
       center,
       zoom: 11,
     });
+    // directionsRenderer.setMap(map);
 
-    locArr.forEach((loc) => {
-      new google.maps.Marker({
+    // const waypoints = locArr.map((el, i, arr) => {
+    //   if (i === 0 || i === arr.length - 1) return;
+    //   return { location: el, stopover: true };
+    // });
+
+    // const req = {
+    //   origin: locArr[0],
+    //   destination: locArr[locArr.length - 1],
+    //   waypoints,
+    //   travelMode: "DRIVING",
+    //   drivingOptions: {
+    //     departureTime: Date.now(),
+    //     trafficModel: "optimistic",
+    //   },
+    // };
+
+    // directionsService.route(req, function (res, status) {
+    //   if (status == "OK") {
+    //     directionsRenderer.setDirections(res);
+    //   }
+    // });
+
+    locArr.forEach((loc, i) => {
+      const contentString = `<div id="content"><h3>${nameArr[i]}</h3></div>`;
+
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+
+      const marker = new google.maps.Marker({
         position: loc,
         map,
+      });
+
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
       });
     });
   }
