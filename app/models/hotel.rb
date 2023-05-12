730f73431ab03ff8e6a2b665007eef47bf5bfd2e
YuTingLan <fortitude0623@gmail.com>
2023-05-12 17:16:45 +0800

    fixed merge issues

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,7 +2,8 @@
 
 class Hotel < ApplicationRecord
   extend FriendlyId
-
+  paginates_per 6
+  
   # 喜愛清單關聯
   has_many :favorites, as: :favorable, dependent: :destroy
   validates :name, :tel, :address, presence: true
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,6 +2,8 @@
 
 class Site < ApplicationRecord
   extend FriendlyId
+  paginates_per 6
+  
   # 喜愛清單關聯
   has_many :favorites, as: :favorable, dependent: :destroy
 
