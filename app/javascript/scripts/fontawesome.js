acbe03389eeb5e0fcbba4cc0b88f752e73c2f793
Betty Hsu <997666aa@gmail.com>
2023-05-14 12:11:48 +0800

    WIP update btn

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -2,8 +2,8 @@
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 config.mutateApproach = 'sync'
 
-import { faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck  } from '@fortawesome/free-solid-svg-icons'
-library.add(faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck )
+import { faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+library.add(faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus)
 
 dom.watch()
 
