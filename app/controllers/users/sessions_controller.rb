47e9a8ce5ab1066295e6e904a19c5f8ab860c4bb
tiger <tiger850109@gmail.com>
2023-05-15 13:38:51 +0800

    minor fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ module Users
       request.referer || root_path
     end
 
-    def after_sign_in_path_for(resource_or_scope)
+    def after_sign_in_path_for(_resource_or_scope)
       if current_user.diamond_grade != '一般會員' && membership_expiry_date < format_date(Time.now)
         current_user.update(diamond_grade: '一般會員')
         flash[:alert] = '會員已到期！'
@@ -15,12 +15,13 @@ module Users
     end
 
     private
+
     def format_date(date)
       date.in_time_zone('Taipei').strftime('%Y/%m/%d')
     end
 
     def membership_expiry_date
       Order.where(user_id: current_user.id).last.pay_time + 1.month
-    end 
+    end
   end
 end
