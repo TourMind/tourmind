a7fcd9e02ba5045774504ef2410087ca15bb1fd0
YuTingLan <fortitude0623@gmail.com>
2023-05-16 03:18:30 +0800

    add_hotels_web_scraping

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -10,8 +10,8 @@ class Hotel < ApplicationRecord
 
   has_many :comments, as: :commentable, dependent: :destroy
   validates :name, :address, presence: true
-  validates :tel, presence: true,
-                  format: { with: /\A(\(\d{2}\)|\d{2})\d{8}\z/, message: '請輸入有效的電話號碼，格式(02)12345678或0912123123' }
+  # validates :tel, presence: true,
+  #                 format: { with: /\A(\(\d{2}\)|\d{2})\d{8}\z/, message: '請輸入有效的電話號碼，格式(02)12345678或0912123123' }
   validates :website, format: { with: URI::DEFAULT_PARSER.make_regexp, message: '請輸入有效的網址' }, if: lambda {
                                                                                                     website.present?
                                                                                                   }
