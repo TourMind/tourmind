8f6d0a3f417622cdca595b9b883dec3fc8873a5b
Betty Hsu <997666aa@gmail.com>
2023-05-19 22:39:42 +0800

    remove the unused controller.js

--- app/javascript/controllers/alert_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-import Swal from "sweetalert2"
-
-// Connects to data-controller="alert"
-export default class extends Controller {
-  connect() {
-    Swal.fire('Title...', '沒有找到符合條件的飯店', 'warning')
-  }
-}
