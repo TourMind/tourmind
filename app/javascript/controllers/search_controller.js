dd209c9ec95556676add3e9f92e93628f4ce425b
Betty Hsu <997666aa@gmail.com>
2023-05-20 18:14:45 +0800

    fix:search reset consider

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,13 +1,26 @@
+
+
+// // Connects to data-controller="search"
+
+
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="search"
 export default class extends Controller {
   static targets = ["keywordInput"]
-  submitForm(e) {
-    const keyword = this.keywordInputTarget.value.trim();
-    if (keyword === "") {
-      e.preventDefault();
+
+  submitForm(event) {
+    event.preventDefault();
+
+    const keywordInput = this.keywordInputTarget;
+    const keyword = keywordInput.value.trim();
+
+    if (keyword !== "") {
+      keywordInput.value = keyword;
+      this.element.submit();
+    } else {
+      console.log("Input field is empty. Form not submitted.");
     }
   }
 }
+
 
