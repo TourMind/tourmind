f00ea670975faa8d7da345f58606fa341dad3160
Betty Hsu <997666aa@gmail.com>
2023-05-24 14:15:56 +0800

    feat:update edit_controller.js

--- app/javascript/controllers/edit_controller.js
+++ app/javascript/controllers/edit_controller.js
@@ -118,11 +118,10 @@ export default class extends Controller {
 
   addDay() {
     const dayElement = `
-    <div class="relative px-4 day min-h-[150px] bg-white flex flex-col justify-center">
+    <div class="relative px-4 day min-h-[150px] bg-white flex flex-col justify-center cursor-grab">
       <div class="relative bg-blue-400 py-3">
-        <h4 class="mr-5 flex justify-center text-xl text-white font-bold day-title">第 ${
-          +this.containerTarget.dataset.days + 1
-        } 天</h4>
+        <h4 class="mr-5 flex justify-center text-xl text-white font-bold day-title">第 ${+this.containerTarget.dataset.days + 1
+      } 天</h4>
         <button data-action="click->edit#deleteDay" type="button" class="rounded-md p-2 flex text-white hover:text-gray-300 absolute right-0 top-1">
           <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -130,8 +129,7 @@ export default class extends Controller {
       </button>
       </div>
       <div class="absolute h-full border border-dashed border-opacity-20 border-slate-600"></div>
-      <div data-controller="sorting" class="h-full w-full sites-list" id="plan-day-${
-        +this.containerTarget.dataset.days + 1
+      <div data-controller="sorting" class="h-full w-full sites-list" id="plan-day-${+this.containerTarget.dataset.days + 1
       }">
       </div>
     </div>`;
