7af215f65bbd7ff7c6c230991d9eae0d0c981dbd
Kevin Yang <e510021@gmail.com>
2023-05-24 16:19:33 +0800

    finished show page

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -46,7 +46,10 @@ export default class extends Controller {
 
     if (res.ok) {
       this.alert("success", "新增到喜愛清單");
-      e.target.closest(".button").innerHTML = this.removeBtn(data.favorite_id);
+      e.target.closest(".button").innerHTML =
+        this.element.id === "show"
+          ? this.longRemoveBtn(data.favorite_id)
+          : this.removeBtn(data.favorite_id);
       return;
     }
 
@@ -56,11 +59,12 @@ export default class extends Controller {
   async removeFav(e) {
     const { id } = e.currentTarget.dataset;
 
-    const res = await destroy(`favorites/${id}`);
+    const res = await destroy(`/favorites/${id}`);
 
     if (res.ok) {
       this.alert("success", "已從喜愛清單中移除");
-      e.target.closest(".button").innerHTML = this.addBtn();
+      e.target.closest(".button").innerHTML =
+        this.element.id === "show" ? this.longAddBtn() : this.addBtn();
       return;
     }
   }
@@ -72,6 +76,14 @@ export default class extends Controller {
     });
   }
 
+  addBtn() {
+    return `
+    <div class="cursor-pointer" data-action="click->favorite#addFav">
+      <span class="text-slate-300 text-4xl">&#x2665;</span>
+    </div>
+    `;
+  }
+
   removeBtn(id) {
     return `
     <div class="cursor-pointer" data-action="click->favorite#removeFav" data-id="${id}">
@@ -80,10 +92,20 @@ export default class extends Controller {
     `;
   }
 
-  addBtn() {
+  longAddBtn() {
     return `
-    <div class="cursor-pointer" data-action="click->favorite#addFav">
-      <span class="text-slate-300 text-4xl">&#x2665;</span>
+    <div class = "text-red-500 cursor-pointer select-none focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-3 py-1 mx-2 bg-white hover:bg-rose-300 hover:text-white border-2" data-action="click->favorite#addFav">
+      <span class="hidden sm:inline-block text-lg">加入喜愛清單</span>
+      <span class="inline-block text-red-500 text-lg">&#x2665;</span>
+    </div>
+    `;
+  }
+
+  longRemoveBtn(id) {
+    return `
+    <div class = "bg-rose-300 cursor-pointer select-none text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-3 py-1 mx-2 hover:bg-white hover:text-red-500 border-2" data-action="click->favorite#removeFav" data-id="${id}">
+      <span class="hidden sm:inline-block text-lg">已加喜愛清單</span>
+      <span class="inline-block text-red-500 text-lg">&#x2665;</span>
     </div>
     `;
   }
