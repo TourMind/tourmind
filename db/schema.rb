fc72671cea0d311eaa68c1e4ba6ab3fc291c4e9e
weihsiang120 <weihsiang120@gmail.com>
2023-05-25 12:27:28 +0800

    add comment delete button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_004845) do
+ActiveRecord::Schema.define(version: 2023_05_24_064520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2023_05_23_004845) do
     t.bigint "user_id"
     t.integer "commentable_id"
     t.string "commentable_type"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_comments_on_deleted_at"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
