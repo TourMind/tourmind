bb51be0cc4a3d76f42a1bd5fe0e1cb39f9f7e674
Betty Hsu <997666aa@gmail.com>
2023-05-25 12:10:27 +0800

    fix:tooltip style

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -2,9 +2,9 @@
 import { config, library, dom } from '@fortawesome/fontawesome-svg-core'
 config.mutateApproach = 'sync'
 
-import { faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus, faComment, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus, faComment, faCircleExclamation, faCircle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus, faComment, faLightbulb)
+library.add(faHeartPulse, faPencil, faLocationDot, faRightToBracket, faCrown, faBars, faChevronDown, faChevronUp, faXmark, faMagnifyingGlass, faGlobe, faHeart, faUsersGear, faStar, faStarHalf, faTimesCircle, faPlus, faCheck, faCirclePlus, faComment, faCircleExclamation, faCircle)
 
 dom.watch()
 
